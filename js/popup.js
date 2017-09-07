905919312ffb888e3fd6ac677230c2a123c56010
dmitry-buyan <buyandl@mail.ru>
2017-09-07 13:25:18 +0400

    Завершение верстки личного проекта

--- js/popup.js
+++ js/popup.js
@@ -1,24 +1,34 @@
 var link = document.querySelector(".write-us");
 var popup = document.querySelector(".modal-feedback");
 var close = document.querySelector(".modal-feedback__close");
-var overlay =document.querySelector(".modal-overlay");
+var overlay = document.querySelector(".modal-overlay");
+var form = popup.querySelector("form");
+var username = popup.querySelector("[name=name]");
+var email = popup.querySelector("[name=email]");
 
-link.addEventListener("click", function(event) {
+link.addEventListener("click", function() {
   event.preventDefault();
-  popup.classList.add("modal-feedback--show");
-  overlay.classList.add("modal-overlay--show");
+  overlay.classList.add("popup-show");
+  popup.classList.add("modal-show");
+  username.focus();
 });
 
-close.addEventListener("click", function(event) {
+close.addEventListener("click", function() {
   event.preventDefault();
-  popup.classList.remove("modal-feedback--show");
-  overlay.classList.remove("modal-overlay--show");
+  overlay.classList.remove("popup-show");
+  popup.classList.remove("modal-show");
 });
 
-window.addEventListener("keydown", function(event) {
-  if(event.keyCode === 27) {
-    if(popup.classList.contains("modal-feedback--show"))
-      popup.classList.remove("modal-feedback--show");
-      overlay.classList.remove("modal-overlay--show");
+window.addEventListener("keydown", function() {
+  if (popup.classList.contains("modal-show")) {
+    popup.classList.remove("modal-show");
+    overlay.classList.remove("popup-show");
+  }
+});
+
+overlay.addEventListener("click", function() {
+  if (popup.classList.contains("modal-show")) {
+    popup.classList.remove("modal-show");
+    overlay.classList.remove("popup-show");
   }
 });
