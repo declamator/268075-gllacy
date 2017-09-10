a549f7d014a32978b246f70a31e604e03a237c7a
Dmitry Buyan <buyandl@mail.ru>
2017-09-10 22:44:22 +0400

    Правка итогового проекта

--- js/popup.js
+++ js/popup.js
@@ -5,6 +5,9 @@ var overlay = document.querySelector(".modal-overlay");
 var form = popup.querySelector("form");
 var username = popup.querySelector("[name=name]");
 var email = popup.querySelector("[name=email]");
+var button = popup.querySelector(".btn");
+var l_user = localStorage.getItem("username");
+var l_email = localStorage.getItem("email");
 
 link.addEventListener("click", function(event) {
   event.preventDefault();
@@ -17,6 +20,7 @@ close.addEventListener("click", function(event) {
   event.preventDefault();
   overlay.classList.remove("popup-show");
   popup.classList.remove("modal-show");
+  popup.classList.remove("modal-error");
 });
 
 window.addEventListener("keydown", function(event) {
@@ -24,6 +28,7 @@ window.addEventListener("keydown", function(event) {
     if (popup.classList.contains("modal-show")) {
       popup.classList.remove("modal-show");
       overlay.classList.remove("popup-show");
+      popup.classList.remove("modal-error");
     }
   }
 });
@@ -32,5 +37,17 @@ overlay.addEventListener("click", function() {
   if (popup.classList.contains("modal-show")) {
     popup.classList.remove("modal-show");
     overlay.classList.remove("popup-show");
+    popup.classList.remove("modal-error");
+  }
+});
+
+form.addEventListener("submit", function(event) {
+  if (!username.value || !email.value) {
+    event.preventDefault();
+    popup.classList.add("modal-error");
+  }
+  else {
+    localStorage.setItem("username", username.value);
+    localStorage.setItem("email", email.value);
   }
 });
